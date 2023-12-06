384d5dceff3ea2710881824b9266e0cb29d3a5b3
Noah-Terve <noahtervalon@gmail.com>
2023-12-05 20:37:29 -0500

    minor updates

--- train.py
+++ train.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 
-# add a new data file here to train on different datay
-results = model.train(data='coco128.yaml', epochs=100, imgsz=640)
+training_path = input("Enter path to .yaml file for testing data: ")
+results = model.train(data=training_path, epochs=100, imgsz=640)
